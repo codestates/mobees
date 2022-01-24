97bdeb6b66b90d9024aa0d08813bc6dbe58e4523
Park shinyeong <syngh503@gmail.com>
2022-01-24 15:45:56 +0900

    Feat:   Make Login feature

--- server/controller/auth/signup.js
+++ server/controller/auth/signup.js
@@ -1,5 +1,41 @@
+const { user } = require("../../models");
+const { generateAccessToken, sendAccessToken } = require("../tokenFunctions");
 
 module.exports = (req, res) => {
+  // 회원 가입 요청을 받습니다.
+  // 이미 존재하는 이메일일 경우 =>  상태코드 409
+  // 회원 정보가 제대로 입력되지 않았으면 => 상태코드 422
 
-    res.send('');
+  const { email, user_name, password, phone_number, nickname } = req.body;
+  if (!email || !user_name || !password || !phone_number || !nickname) {
+    return res
+      .status(422)
+      .send({ message: "Insufficient parameters supplied" });
+  }
+
+  user
+    .findOne({
+      where: {
+        email,
+      },
+    })
+    .then((result) => {
+      if (!result) {
+        user
+          .create({
+            email,
+            password,
+            user_name,
+            phone_number,
+            nickname,
+          })
+          .then((data) => {
+            const accessToken = generateAccessToken(data.dataValues);
+            sendAccessToken(res, accessToken);
+            return res.status(201).send({ message: "ok" });
+          });
+      } else {
+        return res.status(409).send("email.exists");
+      }
+    });
 };
