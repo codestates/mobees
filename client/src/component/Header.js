884604b3dcdb4fbcf0fc09caa94a81066c784385
이재협 <zzok3312@naver.com>
2022-01-24 10:14:04 +0900

    Add: Component

---
+++ client/src/component/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="header_div">
+          <div className="header_left"></div>
+          <div className="header_right">
+            <div className="serch"></div>
+            <div className="login"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
