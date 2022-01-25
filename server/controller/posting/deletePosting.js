f2e4a3d589f2ed10bf491e5cb7024f048b36e1f5
DAEUNY <desmap@gmail.com>
2022-01-26 03:01:58 +0900

    Feat : Delete posting - server
    1. 조인 테이블 레코드 삭제
    2. 요청 포스팅 레코드 삭제

--- server/controller/posting/deletePosting.js
+++ server/controller/posting/deletePosting.js
@@ -1,5 +1,36 @@
 
-module.exports = (req, res) => {
+const { post } = require('../../models')
+const { posts_genres } = require('../../models')
 
-    res.send('');
+module.exports = async (req, res) => {
+  
+  const postId = req.params.postid
+
+  if (!postId) {
+    res.status(400).json({message:'Bad request'})
+  }
+  
+  const isFk = await posts_genres.findOne({
+    where : {
+      post_id : postId
+    }
+  }).catch(err => console.log(err))
+  
+  if (isFk) {
+    await posts_genres.destroy({
+      where : {
+        post_id : postId
+      }
+    }).catch(err => console.log(err))
+  }
+
+  try {
+    await post.destroy({ where : { id : postId } })
+  }
+  catch (err) {
+    console.log(err)
+    res.status(500).json({ message : 'Internal server error' })
+  }
+  
+  res.status(200).json({ message : 'Deleted successfully'})
 };
