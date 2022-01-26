20a3d95d334ffbe33d39f479b43c71127c8c6cfb
DAEUNY <desmap@gmail.com>
2022-01-26 11:53:17 +0900

    Feat : No change
    바뀐 내용 없음 (브랜치 이동)

--- server/controller/myMovie/getMyMovie.js
+++ server/controller/myMovie/getMyMovie.js
@@ -1,3 +1,26 @@
-module.exports = (req, res) => {
-  res.send(''); 
+const { checkAccessToken } = require('../tokenFunctions')
+const { user } = require('../../models')
+const { post } = require('../../models')
+
+module.exports = async (req, res) => {
+  
+  const jwt = checkAccessToken(req).email
+
+  if (!jwt) {
+    res.status(401).json( { data : null, message : 'Not authorized' })
+  }
+  
+  const { email } = jwt
+  if (email) {
+
+    const reqUser = await user.findOne({
+      include : [{model : post}],
+      where : { email },
+    })
+
+    console.log("       -      who     -     0   o   ", reqUser)
+  }
+
+
+  res.status(200).json({ message : 'ok' })
 };
