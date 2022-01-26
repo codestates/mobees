08b8bbb4a6f03001bc42024adc1532efff87dee7
DAEUNY <desmap@gmail.com>
2022-01-26 18:41:18 +0900

    Feat : controller/getMyMovie
    유저의 모든 포스팅
    요청마다 8개씩 전송

--- server/controller/myMovie/getMyMovie.js
+++ server/controller/myMovie/getMyMovie.js
@@ -1,26 +1,51 @@
 const { checkAccessToken } = require('../tokenFunctions')
 const { user } = require('../../models')
-const { post } = require('../../models')
+const { post } = require('../../models');
+// const { use } = require('../../routes/mainMovie');
 
 module.exports = async (req, res) => {
   
-  const jwt = checkAccessToken(req).email
+  const page = req.query.page || 0;
+  const pageAsNumber = Number.parseInt(page);
+  const size = 8;
+  const start = pageAsNumber * size;
+
+  const jwt = checkAccessToken(req)
+  const { email } = jwt
 
   if (!jwt) {
     res.status(401).json( { data : null, message : 'Not authorized' })
   }
   
-  const { email } = jwt
   if (email) {
+    const userData = await user.findOne({
+      where : {
+        email
+      }
+    }).catch(err => console.log(err))
 
-    const reqUser = await user.findOne({
-      include : [{model : post}],
-      where : { email },
+    const usersPosts = await user.findAll({
+      where : {
+        id : userData.id
+      },
+      include : [{ 
+        model : post,
+        attributes: ["id", "image", "total_likes"]
+      }]
+    }).catch(err => console.log(err))
+
+    const postCount = userData.count;
+    if (postCount < start) {
+      res.status(204).send({ message : "Posts no Longer exist" })
+    }
+
+    let myMovie = usersPosts[0].posts
+
+    myMovie = myMovie.slice(start, start + size)
+
+    res.status(200).json({ 
+      data : { myMovie },
+      message : 'ok' 
     })
-
-    console.log("       -      who     -     0   o   ", reqUser)
   }
-
-
-  res.status(200).json({ message : 'ok' })
 };
